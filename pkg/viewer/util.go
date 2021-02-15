6e37cb0b3d4d92b94504d8404249f5cfdd257c96
千橘 雫霞 <qianjunakasumi@gmail.com>
2021-02-15 13:02:04 +0800

    史诗：带 Token 的 POST 请求

--- pkg/viewer/util.go
+++ pkg/viewer/util.go
@@ -1,6 +1,10 @@
 package viewer
 
-import "net/http"
+import (
+	"net/http"
+	"net/url"
+	"strings"
+)
 
 // getWithToken 带 Token 的 GET 请求
 func (v *Viewer) getWithToken(url string) (*http.Response, error) {
@@ -10,6 +14,20 @@ func (v *Viewer) getWithToken(url string) (*http.Response, error) {
 		return nil, err
 	}
 
+	req.Header.Set("cookie", "PHPSESSID="+v.token)
+
+	return http.DefaultClient.Do(req)
+}
+
+// postWithToken 带 Token 的 POST 请求
+func (v *Viewer) postWithToken(url string, data url.Values) (*http.Response, error) {
+
+	req, err := http.NewRequest("POST", url, strings.NewReader(data.Encode()))
+	if err != nil {
+		return nil, err
+	}
+
+	req.Header.Set("Content-Type", "application/x-www-form-urlencoded")
 	req.Header.Set("cookie", "PHPSESSID="+v.token)
 
 	return http.DefaultClient.Do(req)
