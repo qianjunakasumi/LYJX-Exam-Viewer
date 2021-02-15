11e631da9d371411632487d111e2d8cecfc46b91
千橘 雫霞 <qianjunakasumi@gmail.com>
2021-02-15 13:05:47 +0800

    重构：要求输入学籍号和姓名用于处理多个学生档案的情况

--- pkg/viewer/new.go
+++ pkg/viewer/new.go
@@ -4,12 +4,15 @@ package viewer
 type Viewer struct {
 	account  string // 账号
 	password string // 密码
+	number   string // 学籍号
+	name     string // 姓名
 
 	token string // 令牌
 }
 
-// New 返回 Viewer 查看器
-func New(acc string, pwd string) (*Viewer, error) {
-	v := &Viewer{account: acc, password: pwd}
+// New 返回 Viewer 查看器。
+// acc：账号；pwd：密码；num：学籍号；name：姓名
+func New(acc, pwd, num, name string) (*Viewer, error) {
+	v := &Viewer{account: acc, password: pwd, number: num, name: name}
 	return v, v.Refresh()
 }
